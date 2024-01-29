df8d50b2596988c33e55002aece0e4003afeabc5
Thrustgamers <94409067+Thrustgamers@users.noreply.github.com>
2024-01-29 11:07:21 +0100

    file updates

--- server/routes/main.go
+++ server/routes/main.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	loginhandlers "api/handlers/login"
+	userhandlers "api/handlers/users"
+	"api/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -13,4 +15,9 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/logout", loginhandlers.Logout)
 	app.Get("/sessionStorage", loginhandlers.GetUsers)
 
+	user := app.Group("/user", middleware.SessionAuth)
+	user.Get("/get", userhandlers.Get)
+
+	// rank := app.Group("/rank", authentication.AuthMiddleware)
+	// rank.Get("/get", rankhandlers.Get)
 }
