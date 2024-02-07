6eaf0727a4e973380c503ce4d3811bc51735461c
Thrustgamers <oke.leukk@gmail.com>
2024-02-07 23:56:41 +0100

    vite update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,16 @@ import path from 'path';
  
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
+  base: './',
+  preview: {
+    port: 80,
+    strictPort: true
+  },
   resolve: {
     alias: {
       '@style': path.resolve(__dirname, 'styles'),
       '@lib': path.resolve(__dirname, 'lib'),
+      '@utils': path.resolve(__dirname, 'utils'),
     },
   }
 });
