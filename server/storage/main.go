89bc1de0bcd998ee8cd16228867e6308db098d5e
Thrustgamers <oke.leukk@gmail.com>
2024-02-08 17:24:21 +0100

    (BE) added Admin bool to sessionmanager

--- server/storage/main.go
+++ server/storage/main.go
@@ -1,8 +1,9 @@
 package storage
 
 type UserData struct {
-	UserID int `json:"UserID,string"`
-	Token  int `json:"Token,string"`
+	UserID int  `json:"UserID,string"`
+	Token  int  `json:"Token,string"`
+	Admin  bool `json:"Admin,omitempty" default:"false"`
 }
 
 var SessionManager = make(map[int]UserData)
