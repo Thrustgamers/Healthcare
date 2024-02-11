5bd8231d9c689e26bfd3fb71ee3dcd9ab6509cf2
Thrustgamers <oke.leukk@gmail.com>
2024-02-11 23:30:32 +0100

    (BE) start of statuscheck function

--- server/handlers/login/status.go
+++ server/handlers/login/status.go
@@ -1,5 +1,12 @@
 package loginhandlers
 
-func StatusCheck() {
+import (
+	"api/utils"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+func StatusCheck(c *fiber.Ctx) error {
+
+	return utils.SendSuccessResponse(c, "")
 }
