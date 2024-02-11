2592e3e3f2e2d83cee804034e4a5612f252bb44f
Thrustgamers <oke.leukk@gmail.com>
2024-02-11 23:30:13 +0100

    (BE) add employeeid & admin to sessionmanager

--- server/storage/main.go
+++ server/storage/main.go
@@ -1,9 +1,19 @@
 package storage
 
 type UserData struct {
-	UserID int  `json:"UserID,string"`
-	Token  int  `json:"Token,string"`
-	Admin  bool `json:"Admin,omitempty" default:"false"`
+	UserID     int  `json:"UserID,string"`
+	Token      int  `json:"Token,string"`
+	EmployeeId int  `json:"EmployeeId,string"`
+	Admin      bool `json:"Admin,omitempty" default:"false"`
 }
 
 var SessionManager = make(map[int]UserData)
+
+func DoesSessionExist(EmployeeId int) bool {
+	for i := range SessionManager {
+		if SessionManager[i].EmployeeId == EmployeeId {
+			return true
+		}
+	}
+	return false
+}
