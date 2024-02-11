c2666884157808faeb920019a849970bb635f1ac
Thrustgamers <oke.leukk@gmail.com>
2024-02-11 23:31:41 +0100

    (FE) start on status switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import type React from 'react'
+import { server } from "../utils/server"
+import {useAsync} from 'react-use';
+import Login from './login/login';
+import Loading from './loading/loading';
+import Dashboard from './dashboard/dashboard';
 
 
 const App: React.FC = () => {
 
+  const ServerConnection = new server()
+
+  const loggedin = useAsync(async () => {
+    const result = await ServerConnection.auth.checkStatus()
+    return result
+  }, []);
+
+  console.log(loggedin)
+
+  
+
+
   return (
     <>
+      {loggedin.loading ? (
+        < Loading color="dark" />
+      ) : loggedin.value ? (
+        < Dashboard />
+      ) : (
+        <Login />
+      )}
     
     </>
   )
