cc58d4d8cf98b10d5c59ced4dd8d4f00f21964c6
Thrustgamers <oke.leukk@gmail.com>
2024-02-13 22:23:38 +0100

    (BE) New JSON handler

--- server/go.mod
+++ server/go.mod
@@ -49,6 +49,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/gogo/googleapis v1.1.0 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/gogo/status v1.0.3 // indirect
--- server/main.go
+++ server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"api/database"
 	"api/routes"
+	"api/utils"
 	"os"
 
+	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
@@ -29,9 +31,16 @@ func main() {
 		StrictRouting: true,
 		ServerHeader:  "fiber",
 		AppName:       "Healthcare Server v0.0.1",
+		JSONEncoder:   json.Marshal,
+		JSONDecoder:   json.Unmarshal,
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return utils.SendErrorResponse(c, 404, err)
+		},
 	})
 
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+	}))
 	app.Get("/metrics", monitor.New())
 	// app.Static("/", "./web/dist/")
 
