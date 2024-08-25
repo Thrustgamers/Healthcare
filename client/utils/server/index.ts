41ae09609f8627c5d2babc23a4842d0b33c815b4
Thrustgamers <94409067+Thrustgamers@users.noreply.github.com>
2024-08-25 20:42:32 +0200

    reset frontend + vite -> rspack

--- client/utils/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "./auth";
-
-export class server {
-    auth: auth
-
-    constructor() {
-        this.auth = new auth()
-    }
-}
--- client/utils/server/request.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const request = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 1000,
-    withCredentials: true,
-})
